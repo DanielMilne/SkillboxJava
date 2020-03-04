9d98f861eb86aaf9c00a2aa3c366f42ae7d49c97
Daniel <twistsp@gmail.com>
2020-03-04 20:22:10 +0300

    Update Loader.java

--- Модуль 4/StringExperiments/src/Loader.java
+++ Модуль 4/StringExperiments/src/Loader.java
@@ -2,17 +2,47 @@ import java.util.Scanner;
 
 public class Loader {
     public static void main(String[] args) {
-        Scanner name = new Scanner(System.in);
-        System.out.println("Enter Name");
-        String userName = name.nextLine();
-        System.out.println("Enter Family name");
-        String familyName = name.nextLine();
-        System.out.println("Enter Fathers name");
-        String fathersName = name.nextLine();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ФИО:");
+        String fio = scan.nextLine();
 
 
-        System.out.printf("Фамилия: %s \nИмя: %s \nОтчество: %s", userName, familyName, fathersName);
+        if (fio.trim().length() > 0) {
+            int length = fio.trim().length();
+            if (fio.trim().indexOf(' ') == -1) {
+                System.out.println("Фамилия = " + fio);
 
+            } else if (fio.trim().indexOf(' ') > 0) {
+                int space = fio.trim().indexOf(' ');
+                String familia = fio.trim().substring(0, space);
+                System.out.println("Фамилия = " + familia);
+            }
+
+            if (fio.trim().indexOf(' ') > -1) {
+                int space = fio.trim().indexOf(' ');
+                int space2 = fio.trim().lastIndexOf(' ');
+                if (fio.trim().indexOf(' ', space + 1) > -1) {
+                    String name = fio.trim().substring(space, space2);
+                    System.out.println("Имя = " + name);
+                } else {
+                    String name2 = fio.trim().substring(space, length);
+                    System.out.println("Имя = " + name2);
+                }
+
+
+                if (space != space2) {
+                    int lastchr = fio.trim().length();
+                    String otchesvo = fio.trim().substring(space2, lastchr);
+                    System.out.println("Отчество = " + otchesvo);
+                }
+            }
+        } else {
+            System.out.println("пусто");
+
+        }
 
     }
+
 }
+
