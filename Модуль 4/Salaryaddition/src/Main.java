ce4b28581de327707d8bebd2e6fb4e1c7ef368c4
Daniel <twistsp@gmail.com>
2020-03-04 14:08:50 +0300

    Update Main.java

--- Модуль 4/Salaryaddition/src/Main.java
+++ Модуль 4/Salaryaddition/src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         System.out.println(text);
 
-        String Vasja = text.substring(15, 19);   // выделяю из текста число
-        int num = Integer.parseInt(Vasja);      // перевожу в int
-        String Petr = text.substring(35, 39);   // выделяю из текста число
-        int num1 = Integer.parseInt(Petr);      // перевожу в int
-        String Masha = text.substring(56, 61); // выделяю из текста число
-        int num2 = Integer.parseInt(Masha);    // перевожу в int
+        String vasja = text.substring(15, 19);   // выделяю из текста число
+        int num = Integer.parseInt(vasja);      // перевожу в int
+        String petr = text.substring(35, 39);   // выделяю из текста число
+        int num1 = Integer.parseInt(petr);      // перевожу в int
+        String masha = text.substring(56, 61); // выделяю из текста число
+        int num2 = Integer.parseInt(masha);    // перевожу в int
         Integer summ = num + num1 + num2;      // создаю переменную с вычислением общей суммы
         System.out.println("В сумме - " + summ);   // вывожу в консоль
 
