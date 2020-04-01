2cb0fc7fda507572c4ebcd77a1cbe6b1bd792879
Daniel <twistsp@gmail.com>
2020-04-01 13:26:47 +0300

    Update Main.java

--- Модуль 4/Calendar/src/Main.java
+++ Модуль 4/Calendar/src/Main.java
@@ -6,6 +6,34 @@ public class Main {
     public static void main(String[] args) {
 
         int listCount = 0;
+        boolean after;
+        LocalDate now = LocalDate.now();
+        LocalDate birthday = LocalDate.of(1992, 6, 5);
+        while (after = birthday.isBefore(now)) {
+            //        LocalDate Birthday = LocalDate.of(1992, 6, 5);
+            //     LocalDate Birthday1 = LocalDate.of(1992, 6, 5);
+            LocalDate minusYears = birthday.plusYears(1); // 2020-01-21T09:11:48.486298  // понял как это работет
+            System.out.println(birthday.format(DateTimeFormatter.ofPattern(listCount + " - " + "dd.MM.yyyy - E")));
+            listCount++;
+            birthday = minusYears;
+        }
+    }
+
+
+}
+
+
+/*
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int listCount = 0;
         int yearOfBirth = 1992;
         while (yearOfBirth < 2021) {
             LocalDate startYear = LocalDate.of(yearOfBirth, 06, 05); // 2018-01-21T09:11:48.486298
@@ -20,7 +48,7 @@ public class Main {
 
 
 
-/*import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
