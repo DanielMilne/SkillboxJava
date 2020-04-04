42e09669966fad2455fa26515fd2527595686548
Daniel <twistsp@gmail.com>
2020-04-04 21:17:08 +0300

    Update Main.java

--- Модуль 5/hospital/src/Main.java
+++ Модуль 5/hospital/src/Main.java
@@ -1,6 +1,11 @@
 import java.text.DecimalFormat;
 
 public class Main {
+    public static final double MAX_HEALTHY_TEMP = 36.90;
+    public static final double MIN_HEALTHY_TEMP = 36.20;
+    public static final double MIN_TEMP = 32.00;
+    public static final double MAX_TEMP = 40.00;
+
 
     public static void main(String[] args) {
 //Создать массив с температурами 30-ти пациентов (от 32 до 40 градусов).
@@ -8,7 +13,7 @@ public class Main {
         float temprNumber = 0;
         int healthyPeople = 0;
         for (int i = 0; i < temperature.length; i++) {
-            float randomInt = (float) (32 + Math.random() * 8);
+            float randomInt = (float) (MIN_TEMP + Math.random() * (MAX_TEMP - MIN_TEMP));
             temperature[i] = randomInt;
         }
 //Написать код, рассчитывающий среднюю температуру по больнице
@@ -22,7 +27,7 @@ public class Main {
 
 //количество здоровых пациентов (с температурой от 36,2 до 36,9)
         for (int i = 0; i < temperature.length; i++) {
-            if ((36.2 < temperature[i]) && (temperature[i] < 36.9)) {
+            if ((MIN_HEALTHY_TEMP < temperature[i]) && (temperature[i] < MAX_HEALTHY_TEMP)) {
                 healthyPeople++;
             }
         }
