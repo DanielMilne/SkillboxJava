44ff1413b0826babcd48dfe3b1d3660f7a86d8c8
Daniel <twistsp@gmail.com>
2020-04-27 18:49:37 +0300

    Update Main.java

--- Модуль 5/todoolist/src/Main.java
+++ Модуль 5/todoolist/src/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    private static ArrayList<String> todoList = new ArrayList<String>(1000) {{
+    private static ArrayList<String> todoList = new ArrayList<String>() {{
         add("первое дело");
         add("второе дело");
     }};
@@ -16,10 +16,10 @@ public class Main {
             System.out.println("Введите команду");
 
             String command = scan.nextLine();
-            Matcher matchAdd = Pattern.compile("^ADD (?<text>.+)").matcher(command);
-            Matcher matchAddIndexed = Pattern.compile("^ADD (?<dealNo>\\d+) (?<text>.+)").matcher(command);
-            Matcher matchDELETEIndexed = Pattern.compile("^DELETE (?<dealNo>\\d+)").matcher(command);
-            Matcher matchEDITIndexed = Pattern.compile("^EDIT (?<dealNo>\\d+) (?<text>.+)").matcher(command);
+            Matcher matchAdd = Pattern.compile("ADD (?<text>.+)").matcher(command);
+            Matcher matchAddIndexed = Pattern.compile("ADD (?<dealNo>\\d+) (?<text>.+)").matcher(command);
+            Matcher matchDELETEIndexed = Pattern.compile("DELETE (?<dealNo>\\d+)").matcher(command);
+            Matcher matchEDITIndexed = Pattern.compile("EDIT (?<dealNo>\\d+) (?<text>.+)").matcher(command);
 
             if (matchAddIndexed.matches()) {
                 System.out.println("Команда добавения дела с индексом");
@@ -53,56 +53,18 @@ public class Main {
         }
     }
 
-    private static void printList(ArrayList<String> args) {
-        for (int i = 0; i < Main.todoList.size(); i++) {
-            System.out.println(i + " - " + Main.todoList.get(i));
+    private static void printList(ArrayList<String> todoList) {
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i + " - " + todoList.get(i));
         }
     }
 
     public static int parseIndex(Matcher a) {
         int indexCheck = Integer.parseInt(a.group("dealNo"));
-        if (indexCheck > Main.todoList.size()) {
+        if (indexCheck > todoList.size()) {
             System.out.println("неверный индекс");
             indexCheck = -1;
         }
         return indexCheck;
     }
 }
-
-
-//            } else if (command.matches("ADD\\s\\d*\\s.*")) {
-//                String index = command.replaceAll("[^0-9]", "");
-//                String addFunction = command.replaceAll("ADD\\s\\d*\\s", "");
-//                todoList.add(Integer.parseInt(index), addFunction);
-//               //  добавление дела
-//            } else if (command.matches("ADD\\s\\w*")) {
-//                String addFunctionIndex = command.replaceAll("ADD\\s", "");
-//                todoList.add(addFunctionIndex);
-//   изменение дела
-//            if (command.matches("EDIT\\s\\d*.*")) {
-//                String index = command.replaceAll("[^0-9]", "");
-//                if (Integer.parseInt(index) > todoList.size()) {
-//                    System.out.println("wrong index");
-//                } else {
-//                    String editFunction = command.replaceAll("EDIT\\s\\d*\\s", "");
-//                    todoList.set(Integer.parseInt(index), editFunction);
-//                }
-//                // вывод в консоль списка
-
-
-// удаление дела из списка
-//            if (command.matches("DELETE\\s\\d*")) {
-//                String index = command.replaceAll("[^0-9]", "");
-//                if (Integer.parseInt(index) > todoList.size()) {
-//                    System.out.println("wrong index");
-//                } else {
-//                    int result = Integer.parseInt(index);
-//                    todoList.remove(result);
-//                }
-//            }
-
-
-//                }
-//                String index1 = matchAddIndexed.group("dealNo");
-//                String addFunction = matchAddIndexed.group("text");
-//                todoList.add(Integer.parseInt(index1), addFunction);
